2692329a7bef664b61888a49139f6cf5c0d7df32
kolisnichenko2828 <kolisnichenko2828@gmail.com>
2026-03-04 11:58:14 +0200

    initial commit

---
+++ app/src/main/java/com/kolisnichenko2828/randomusers/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.kolisnichenko2828.randomusers
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.kolisnichenko2828.randomusers.presentation.navigation.RandomUsersApp
+import com.kolisnichenko2828.randomusers.presentation.theme.RandomUsersTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            RandomUsersTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+                    RandomUsersApp(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/kolisnichenko2828/randomusers/presentation/navigation/RandomUsersApp.kt
@@ -0,0 +1,10 @@
+package com.kolisnichenko2828.randomusers.presentation.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun RandomUsersApp(
+    modifier: Modifier = Modifier
+) {
+}
