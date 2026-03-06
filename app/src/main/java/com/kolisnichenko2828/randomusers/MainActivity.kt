4d9a2a06ecb9da2ec19bb23ae5b5de8c2c49f144
kolisnichenko2828 <kolisnichenko2828@gmail.com>
2026-03-06 09:55:59 +0200

    add: splash screen, icon

--- app/src/main/java/com/kolisnichenko2828/randomusers/MainActivity.kt
+++ app/src/main/java/com/kolisnichenko2828/randomusers/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.kolisnichenko2828.randomusers.presentation.navigation.RandomUsersApp
 import com.kolisnichenko2828.randomusers.presentation.theme.RandomUsersTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -15,6 +16,7 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
