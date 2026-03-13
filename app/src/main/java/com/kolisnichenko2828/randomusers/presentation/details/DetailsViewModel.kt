152c8700c6a3bc54d49bde753590e858eb2de22f
kolisnichenko2828 <kolisnichenko2828@gmail.com>
2026-03-13 12:24:16 +0200

    refactor: fix uuid update in DetailsScreen and improve pagination logic

--- app/src/main/java/com/kolisnichenko2828/randomusers/presentation/details/DetailsViewModel.kt
+++ app/src/main/java/com/kolisnichenko2828/randomusers/presentation/details/DetailsViewModel.kt
@@ -7,6 +7,7 @@ import com.kolisnichenko2828.randomusers.core.StateHolderImpl
 import com.kolisnichenko2828.randomusers.core.onStartCollectingState
 import com.kolisnichenko2828.randomusers.domain.interfaces.UsersDetailsFetcher
 import com.kolisnichenko2828.randomusers.domain.mappers.toDetailsUiModel
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import org.koin.android.annotation.KoinViewModel
 import org.koin.core.annotation.InjectedParam
@@ -17,6 +18,8 @@ class DetailsViewModel(
     @InjectedParam private val uuid: String
 ) : ViewModel(),
     StateHolder<DetailsContract.State> by StateHolderImpl(DetailsContract.State()) {
+
+    private var job: Job? = null
 
     init {
         onStartCollectingState {
@@ -35,7 +38,9 @@ class DetailsViewModel(
 
         updateState { it.copy(error = null) }
 
-        viewModelScope.launch {
+        job?.cancel()
+
+        job = viewModelScope.launch {
             val result = repository.getUser(uuid)
 
             result.fold(
--- app/src/main/java/com/kolisnichenko2828/randomusers/presentation/users/UsersViewModel.kt
+++ app/src/main/java/com/kolisnichenko2828/randomusers/presentation/users/UsersViewModel.kt
@@ -7,6 +7,7 @@ import com.kolisnichenko2828.randomusers.core.StateHolderImpl
 import com.kolisnichenko2828.randomusers.core.onStartCollectingState
 import com.kolisnichenko2828.randomusers.domain.interfaces.UsersListFetcher
 import com.kolisnichenko2828.randomusers.domain.mappers.toUiModels
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import org.koin.android.annotation.KoinViewModel
 
@@ -15,6 +16,8 @@ class UsersViewModel(
     private val repository: UsersListFetcher
 ) : ViewModel(),
     StateHolder<UsersContract.State> by StateHolderImpl(UsersContract.State()) {
+
+    private var job: Job? = null
 
     init {
         onStartCollectingState {
@@ -34,7 +37,9 @@ class UsersViewModel(
     private fun loadInitial(limit: Int = 30) {
         if (uiState.value.isLoadingInitial) return
 
-        viewModelScope.launch {
+        job?.cancel()
+
+        job = viewModelScope.launch {
             updateState {
                 it.copy(
                     error = null,
@@ -79,7 +84,9 @@ class UsersViewModel(
     private fun loadNext(limit: Int = 30) {
         if (uiState.value.isLoadingNext) return
 
-        viewModelScope.launch {
+        job?.cancel()
+
+        job = viewModelScope.launch {
             updateState {
                 it.copy(
                     error = null,
@@ -117,7 +124,9 @@ class UsersViewModel(
     fun refresh(limit: Int = 30) {
         if (uiState.value.isRefreshing) return
 
-        viewModelScope.launch {
+        job?.cancel()
+
+        job = viewModelScope.launch {
             updateState {
                 it.copy(
                     error = null,
