049259467824cd8a6f995c9f9f5c4298a1735381
kolisnichenko2828 <kolisnichenko2828@gmail.com>
2026-03-13 10:11:19 +0200

    refactor: use test for koin annotations navigation graph

--- app/src/test/java/com/kolisnichenko2828/randomusers/di/KoinGraphTest.kt
+++ app/src/test/java/com/kolisnichenko2828/randomusers/di/KoinGraphTest.kt
@@ -1,9 +1,10 @@
 package com.kolisnichenko2828.randomusers.di
 
 import android.content.Context
+import com.kolisnichenko2828.randomusers.AppModule
 import org.junit.Test
 import org.koin.core.annotation.KoinExperimentalAPI
-import org.koin.dsl.module
+import org.koin.ksp.generated.module
 import org.koin.test.KoinTest
 import org.koin.test.verify.verify
 
@@ -11,14 +12,7 @@ import org.koin.test.verify.verify
 class KoinGraphTest : KoinTest {
     @Test
     fun checkKoinModules() {
-        val allModules = module {
-            includes(
-                localModule,
-                remoteModule,
-                repositoryModule,
-                presentationModule
-            )
-        }
+        val allModules = AppModule().module
 
         allModules.verify(
             extraTypes = listOf(
